3daacdef0dcc1588e6dc594ce7d399b619d03df1
wetts <zhang.wetts@163.com>
2016-06-28 17:51:36 +0800

    create project

---
+++ application/src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * Created by linger on 2016/6/28.
+ */
+public class Test {
+
+    public void test() {
+
+    }
+
+}
---
+++ module-auth/src/main/java/com/wetts/Ttest.java
@@ -0,0 +1,13 @@
+package com.wetts;
+
+/**
+ * Hello world!
+ *
+ */
+public class Ttest
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
