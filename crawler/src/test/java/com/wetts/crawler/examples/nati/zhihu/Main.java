396050a1d24f4f84aecb76178c28806b6844d448
wetts <zhang.wetts@163.com>
2016-07-24 16:01:13 +0800

    add crawler4j demo

---
+++ crawler/src/test/java/com/wetts/crawler/examples/nati/zhihu/Main.java
@@ -0,0 +1,20 @@
+package com.wetts.crawler.examples.nati.zhihu;
+
+import java.util.ArrayList;
+
+/**
+ * Created by linger on 2016/7/20.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // 定义即将访问的链接
+        String url = "http://www.zhihu.com/explore/recommendations";
+        // 访问链接并获取页面内容
+        String content = Spider.SendGet(url);
+        // 获取编辑推荐
+        ArrayList<Zhihu> myZhihu = Spider.GetRecommendations(content);
+        // 打印结果
+        System.out.println(myZhihu);
+    }
+}
