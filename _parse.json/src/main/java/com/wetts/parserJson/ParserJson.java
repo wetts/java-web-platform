290214e0c820d68f923bb56ef9ce6db697bbc283
wetts <zhang.wetts@163.com>
2016-09-01 00:06:26 +0800

    modify path

--- _parse.json/src/main/java/com/wetts/parserJson/ParserJson.java
+++
@@ -1,8 +0,0 @@
-package com.wetts.parserjson;
-
-public interface ParserJson {
-
-	public Person parserJsonString(String json);
-	
-	public String createJsonString(Person person);
-}
--- _parse.json/src/main/java/com/wetts/parserJson/jackson/annotation/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author wetts
- * @created 2015/06/04
- *
- */
-package com.wetts.parserjson.jackson.annotation;
